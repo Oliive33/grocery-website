de9ee8f11fdb38360263e526e720596178550587
olivier villevieille <oliviervillevieille@hotmail.fr>
2022-03-24 09:30:44 +0100

    modif counter

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ window.onscroll = () => {
   }
 };
 
-let countDate = new Date("june 1, 2021 00:00:00").getTime();
+let countDate = new Date("March 30, 2022 00:00:00").getTime();
 
 function CountDown() {
   let now = new Date().getTime();
@@ -27,14 +27,14 @@ function CountDown() {
   let second = 1000;
   let minute = second * 60;
   let hour = minute * 60;
-  // let day = hour * 24;
+  let day = hour * 24;
 
-  // let d = Math.floor(gap / (day));
+  let d = Math.floor(gap / day);
   let h = Math.floor(gap / hour);
   let m = Math.floor((gap % hour) / minute);
   let s = Math.floor((gap % minute) / second);
 
-  // document.getElementById('day').innerText = d;
+  document.getElementById("day").innerText = d;
   document.getElementById("hour").innerText = h;
   document.getElementById("minute").innerText = m;
   document.getElementById("second").innerText = s;
